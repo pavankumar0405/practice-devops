672839531cb8b6542081867a6f397c86c28666ea
Pavan <dharavathpavabkumar1101@gmmisl.com>
2026-02-20 15:58:52 +0530

    Done4

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_key_pair" "mykey" {
     key_name = "pkkey"
     public_key = file("~/.ssh/id_ed25519.pub")
 }
-resource "aws_vpc" "myec2" {
+resource "aws_instance" "myec2" {
     ami = "ami-019715e0d74f695be"
     instance_type = "t3.micro"
     key_name = aws_key_pair.mykey.key_name
